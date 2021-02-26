6eacb1687906f839f7b22b3ba511993542fc363b
Marco <marcoip@stud.ntnu.no>
2021-02-26 14:58:21 +0100

    almost done missing uptime

--- main.go
+++ main.go
@@ -3,47 +3,229 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-type article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"Desc"`
-	Content string `json:"Content"`
+// A Response struct to restcountries
+type Response struct {
+	Country    string       `json:"name"`
+	Currencies []Currency   `json:"currencies"`
+	Border     []string     `json:"borders"`
+	Exchange   ExchangeData `json:exchangedata`
 }
 
-type articles []article
+type Currency struct {
+	Code string `json:"code"`
+}
 
-func exchangeHistory(rw http.ResponseWriter, r *http.Request) {
-	articles := articles{
-		article{Title: "Article 1", Desc: "Description 1", Content: " Content 1"},
-		article{Title: "Article 2", Desc: "Description 2", Content: " Content 2"},
-		article{Title: "Article 3", Desc: "Description 3", Content: " Content 3"},
+type ExchangeData struct {
+	Rates Rate   `json:"rates"`
+	Name  string `json:"name"`
+	Base  string `json:"base"`
+	Date  string `json:"date"`
+}
+
+type Rate struct {
+	CAD float64 `json:"CAD,omitempty"`
+	HKD float64 `json:"HKD,omitempty"`
+	ISK float64 `json:"ISK,omitempty"`
+	PHP float64 `json:"PHP,omitempty"`
+	DKK float64 `json:"DKK,omitempty"`
+	HUF float64 `json:"HUF,omitempty"`
+	CZK float64 `json:"CZK,omitempty"`
+	AUD float64 `json:"AUD,omitempty"`
+	RON float64 `json:"RON,omitempty"`
+	SEK float64 `json:"SEK,omitempty"`
+	IDR float64 `json:"IDR,omitempty"`
+	INR float64 `json:"INR,omitempty"`
+	BRL float64 `json:"BRL,omitempty"`
+	RUB float64 `json:"RUB,omitempty"`
+	HRK float64 `json:"HRK,omitempty"`
+	JPY float64 `json:"JPY,omitempty"`
+	THB float64 `json:"THB,omitempty"`
+	CHF float64 `json:"CHF,omitempty"`
+	SGD float64 `json:"SGD,omitempty"`
+	PLN float64 `json:"PLN,omitempty"`
+	BGN float64 `json:"BGN,omitempty"`
+	TRY float64 `json:"TRY,omitempty"`
+	CNY float64 `json:"CNY,omitempty"`
+	NOK float64 `json:"NOK,omitempty"`
+	NZD float64 `json:"NZD,omitempty"`
+	ZAR float64 `json:"ZAR,omitempty"`
+	USD float64 `json:"USD,omitempty"`
+	MXN float64 `json:"MXN,omitempty"`
+	ILS float64 `json:"ILS,omitempty"`
+	GBP float64 `json:"GBP,omitempty"`
+	KRW float64 `json:"KRW,omitempty"`
+	MYR float64 `json:"MYR,omitempty"`
+	EUR float64 `json:"EUR,omitempty"`
+}
+
+type Final struct {
+	Rate []Data `json:"rates"`
+}
+
+type Data struct {
+	Name     string `json:"name"`
+	Currency string `json:"currency"`
+	Rate     Rate   `json:"rate"`
+	Base     string `json:"base"`
+}
+
+type Diagnostic struct {
+	ExchangeRateAPI int    `json:"exchangerateapi"`
+	RestCountries   int    `json:"restcountries"`
+	Version         string `json:"version"`
+	Uptime          string `json:"uptime"`
+}
+
+//// Gets the exchange history of a provided country with provided start and end date
+func exchangeHistory(w http.ResponseWriter, r *http.Request) {
+	search := strings.Split(r.URL.Path, "/")
+	country := strings.Join(search[4:5], "")
+	temp := strings.Split(strings.Join(search[5:], ""), "-")
+	startdate := strings.Join(temp[:3], "-")
+	enddate := strings.Join(temp[3:], "-")
+
+	var req = "https://restcountries.eu/rest/v2/name/" + country + "?fields=currencies"
+
+	respcurr, err := http.Get(req)
+	if err != nil {
+		log.Fatalln(err)
 	}
-	fmt.Println("Endpoint Hint: All Articles Endpoint")
 
-	rw.Header().Set("Content-Type", "application/json")
-
-	json.NewEncoder(rw).Encode(articles)
-}
-
-func exchangeBorder(rw http.ResponseWriter, r *http.Request) {
-	articles := articles{
-		article{Title: "Article 1", Desc: "Description 1", Content: " Content 1"},
-		article{Title: "Article 2", Desc: "Description 2", Content: " Content 2"},
-		article{Title: "Article 3", Desc: "Description 3", Content: " Content 3"},
+	bodycurr, err := ioutil.ReadAll(respcurr.Body)
+	if err != nil {
+		log.Fatalln(err)
 	}
-	fmt.Println("Endpoint Hint: All Articles Endpoint")
+	var curr []Response
+	json.Unmarshal(bodycurr, &curr)
 
-	rw.Header().Set("Content-Type", "application/json")
+	req = "https://api.exchangeratesapi.io/history?start_at=" + startdate + "&end_at=" + enddate + "&symbols=" + curr[0].Currencies[0].Code
+	if curr[0].Currencies[0].Code == "EUR" {
+		req += "&base=USD"
+	}
 
-	json.NewEncoder(rw).Encode(articles)
+	resp, err := http.Get(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	exrate := json.RawMessage(body)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(exrate)
 }
 
-func diagnostics(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(rw, "Hello Prog2005, Here we go")
+func exchangeBorder(w http.ResponseWriter, r *http.Request) {
+	search := strings.Split(r.URL.Path, "/")
+	country := strings.Join(search[4:], "")
+
+	url := "https://restcountries.eu/rest/v2/name/" + country
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject []Response
+	json.Unmarshal(responseData, &responseObject)
+
+	var data []Data
+	var base string
+
+	for i := 0; i < len(responseObject[0].Border); i++ {
+		Borders := responseObject[0].Border[i]
+
+		url1 := "https://restcountries.eu/rest/v2/alpha?codes=" + Borders
+
+		responseCountry, err := http.Get(url1)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseDataCountry, err := ioutil.ReadAll(responseCountry.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var responseObjectCountry []Response
+		json.Unmarshal(responseDataCountry, &responseObjectCountry)
+
+		url2 := "https://api.exchangeratesapi.io/latest?symbols=" + responseObjectCountry[0].Currencies[0].Code
+		if responseObjectCountry[0].Currencies[0].Code == responseObject[0].Currencies[0].Code {
+			if responseObjectCountry[0].Currencies[0].Code == "EUR" {
+				url2 += "&base=USD"
+				base = "USD"
+			} else {
+				url2 += "&base=EUR"
+				base = "EUR"
+			}
+		} else {
+			url2 += "&base=" + responseObject[0].Currencies[0].Code
+			base = responseObject[0].Currencies[0].Code
+		}
+
+		responseExchange, err := http.Get(url2)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseDataExchange, err := ioutil.ReadAll(responseExchange.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var responseObjectExchange ExchangeData
+		json.Unmarshal(responseDataExchange, &responseObjectExchange)
+
+		responseObjectCountry[0].Exchange = responseObjectExchange
+
+		data = append(data, Data{Name: responseObjectCountry[0].Country, Currency: responseObjectCountry[0].Currencies[0].Code, Rate: responseObjectCountry[0].Exchange.Rates, Base: base})
+
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(data)
+}
+
+func diagnostics(w http.ResponseWriter, r *http.Request) {
+
+	responseEx, err := http.Get("https://api.exchangeratesapi.io")
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseCount, err := http.Get("https://restcountries.eu")
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	diagnostic := Diagnostic{ExchangeRateAPI: responseEx.StatusCode, RestCountries: responseCount.StatusCode, Version: "v1"}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(diagnostic)
 }
 
 func homePage(rw http.ResponseWriter, r *http.Request) {
